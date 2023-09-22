ac968204165118305715f9d3417c307e86c8746b
Pratik <79096422+prateiku@users.noreply.github.com>
2023-09-23 02:25:06 +0530

    added util function for parsing json

--- src/apifairy/core.py
+++ src/apifairy/core.py
@@ -25,6 +25,17 @@ from apifairy.decorators import _webhooks
 from apifairy.exceptions import ValidationError
 from apifairy import fields as apifairy_fields
 
+def parse_and_sort(json_obj, key_to_sort_by="name"):
+    if isinstance(json_obj, list):
+        if key_to_sort_by is not None and all(isinstance(item, dict) for item in json_obj):
+            json_obj = sorted(json_obj, key=lambda x: x.get(key_to_sort_by, ""))
+        for i, item in enumerate(json_obj):
+            json_obj[i] = parse_and_sort(item, key_to_sort_by)
+    elif isinstance(json_obj, dict):
+        for key, value in json_obj.items():
+            json_obj[key] = parse_and_sort(value, key_to_sort_by)
+    return json_obj
+
 
 class APIFairy:
     def __init__(self, app=None):
@@ -57,22 +68,7 @@ class APIFairy:
 
         if self.apispec_path:
             def json():
-                json_obj = self.apispec
-                print(""""
-                      
-                      jbkjbjhbhjbjhbjbh
-                      
-                      """)
-                key_to_sort_by = "name"
-                if isinstance(json_obj, list):
-                    if key_to_sort_by is not None and all(isinstance(item, dict) for item in json_obj):
-                        # Sort the list of dictionaries by the specified key
-                        json_obj = sorted(json_obj, key=lambda x: x.get(key_to_sort_by, ""))
-                    for i, item in enumerate(json_obj):
-                        json_obj[i] = parse_and_sort(item, key_to_sort_by)
-                elif isinstance(json_obj, dict):
-                    for key, value in json_obj.items():
-                        json_obj[key] = parse_and_sort(value, key_to_sort_by)
+                json_obj = parse_and_sort(self.apispec)
                 return dumps(json_obj), 200, \
                     {'Content-Type': 'application/json'}
 
