061a7269027b691abc28ae8d2ade98f160d248ec
Pratik <79096422+prateiku@users.noreply.github.com>
2023-09-26 14:04:22 +0530

    Update utils.py

--- src/apifairy/utils.py
+++ src/apifairy/utils.py
@@ -3,6 +3,7 @@ def sort_parameters_by_name(_item: dict) -> dict:
     for k, v in item.items():
         for method in ['get', 'post', 'put', 'patch', 'delete']:
             parameters = v.get(method, {}).get('parameters', [])
-            sorted_parameters = sorted(parameters, key=lambda param: param.get('name', ''))
-            v.setdefault(method, {})['parameters'] = sorted_parameters
+            if parameters:
+                sorted_parameters = sorted(parameters, key=lambda param: param.get('name', ''))
+                v.setdefault(method, {})['parameters'] = sorted_parameters
     return _item
